344d5db005ad6e51c98e532a3b092e1e4cccaa28
Sergio Hernández Mancebo <shernandez@encamina.com>
2018-10-15 23:35:11 +0200

    maps

---
+++ SqlSaturday.AzureMaps/SqlSaturday.AzureMaps.Api/Controllers/MapsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SqlSaturday.AzureMaps.Api.Controllers
+{
+    [Route("api/Maps")]
+    [ApiController]
+    public class MapsController : ControllerBase
+    {
+       
+        [HttpPost("Upload")]
+        public void UploadFile([FromBody] byte[] data)
+        {
+           
+            
+        }
+    }
+}
